ae881b5dc77d94100b592c2283a47d18c0458a0e
Alfonso Manuel Baena González <alfonsombaena@gmail.com>
2024-08-01 23:59:33 +0200

    :sparkles: feat: use puppeteer to generate pdf in order to have selectable resume

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -2,12 +2,14 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--disable-dev-shm-usage']
+    });
     const page = await browser.newPage();
 
-    const filePath = path.resolve(__dirname, 'cv.html');
+    const filePath = path.resolve(__dirname, '../resume.html');
     const fileUrl = `file://${filePath}`;
-
     await page.goto(fileUrl, { waitUntil: 'networkidle2' });
     await page.pdf({
         path: 'resume.pdf',
